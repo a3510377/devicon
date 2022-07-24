50353516dd0f067f557db9cdbc982679ae500b06
猴子 <a102009102009@gmail.com>
2022-07-24 17:59:00 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-
-import { getDeviconData, deviconDataType } from '@/utils/data';
 import IconsComponent from '@/components/home/IconsComponent.vue';
 import IconInfo from '@/components/home/IconInfo.vue';
 import CustomizationComponent from '@/components/home/CustomizationComponent.vue';
 import FooterComponent from '@/components/base/FooterComponent.vue';
-
-const deviconData = reactive<deviconDataType[]>([]);
-getDeviconData().then((d) => Object.assign(deviconData, d));
 </script>
 
 <template>
